01e5ea343d913ed70d31e62d083ae0bfd4448690
Rotimi Bolorunduro <rotimibolorunduro@gmail.com>
2022-02-02 15:09:26 +0100

    Added universal global styling for the app

---
+++ src/Styles.js
@@ -0,0 +1,87 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#ddd',
+      alignItems: 'center',
+      paddingTop: 20,
+      textAlign: 'center',
+    },
+    main: {
+      width: '90%',
+    },
+    heading: {
+      fontSize: 20,
+      fontWeight: '700',
+      paddingTop: 50,
+      textAlign: 'center',
+    },
+    subtext: {
+      fontSize: 14,
+      fontWeight: '400',
+      padding: 10,
+      textAlign: 'center',
+    },
+    input: {
+      borderWidth: 1,
+      padding: 10,
+      textAlign: 'center',
+      backgroundColor: '#ccc',
+    },
+    card: {
+      backgroundColor: '#fff',
+      padding: 10,
+      fontSize: 14,
+      marginTop: 20,
+      shadowColor: '#333',
+      borderRadius: 5,
+    },
+    mood: {
+      fontSize: 30,
+    },
+    name: {
+      fontSize: 16,
+      fontWeight: '700',
+      padding: 10,
+      textAlign: 'left',
+    },
+    position: {
+      fontSize: 14,
+      fontWeight: '600',
+      padding: 10,
+      textAlign: 'left',
+    },
+    good: {
+      color: 'green',
+      fontSize: 14,
+      fontWeight: '400',
+      padding: 10,
+      textAlign: 'center',
+    },
+    bad: {
+      color: 'red',
+      fontSize: 14,
+      fontWeight: '400',
+      padding: 10,
+      textAlign: 'center',
+    },
+    file: {
+      backgroundColor: '#333',
+      color: '#fff',
+      alignItems: 'center',
+      borderRadius: 5,
+      marginLeft: 35,
+      marginRight: 35,
+      marginTop: 15,
+    },
+    button: {
+      color: '#fff',
+      paddingVertical: 10,
+      fontSize: 16,
+    },
+    empty: {
+      marginBottom: 100,
+    },
+  });
+  
