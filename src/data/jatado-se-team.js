1e755bf7e28085b4c1f56bfd8dd5ffc07abf4682
Rotimi Bolorunduro <rotimibolorunduro@gmail.com>
2022-02-02 15:10:28 +0100

    Added datasets for Jatodian SEs

---
+++ src/data/jatado-se-team.js
@@ -0,0 +1,23 @@
+export const JatodianSEs = [
+    {
+      "name": "Ojonugwa Victory Oji",
+      "position": "Grand Cheif Jatado / Lead Software Engineer",
+      "mood": "😍"
+    },
+    {
+      "name": "Ayomide Oyediran",
+      "position": "Software Engineer",
+      "mood": "😊"
+    },
+    {
+      "name": "Peter Chukwu",
+      "position": "Software Engineer",
+      "mood": "😎"
+    },
+    {
+      "name": "Rotimi Victor Bolorunduro",
+      "position": "Software Engineer",
+      "mood": "🤔"
+    }
+  ];
+  
